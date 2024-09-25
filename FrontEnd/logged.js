63ac4d13cfa4ff2c62d454079fc42cd89c66015e
stephaneM <moyartstephane@gmail.com>
2024-09-25 20:23:34 +0200

    modif login / logout + global pre-modal

--- FrontEnd/logged.js
+++ FrontEnd/logged.js
@@ -6,20 +6,48 @@ function getCookie(name) {
         else return null
 }
 
-function inCaseLogged () {
+function inCaseLoggedForLogoutDisplay () {
+    
     if (getCookie("acces") === "pass") {
-        const mainContainer = document.querySelector('.mainContainer')
-        mainContainer.remove()
-        const newContainer = document.createElement('button')
-        newContainer.textContent = "Deconnexion"
-        const main = document.querySelector('main')
-        main.append(newContainer)
-
-        newContainer.addEventListener('click', () => {
+        const logout = document.querySelector('nav ul li:nth-of-type(3)')
+        logout.innerHTML = `<a href="index.html">logout</a>`
+        
+        logout.addEventListener('click', () => {
             document.cookie = "acces=; expires=Thu, 01 Jan 2000 00:00:00 GMT; path=/FrontEnd" 
             location.reload()
         })
     }    
 }
 
-inCaseLogged()
+inCaseLoggedForLogoutDisplay()
+
+function inCaseLoggedForEditHeader () {
+    if (getCookie("acces") === "pass") {
+        const editHeader = document.createElement('div')
+        editHeader.classList.add('editHeader')
+
+        const editHeaderText = document.createElement('p')
+        editHeaderText.innerHTML = "<p><i class='fa-regular fa-pen-to-square'></i>mode édition</p>"
+
+        const html = document.querySelector('html')
+        editHeader.append(editHeaderText)
+        html.prepend(editHeader)
+    }
+}
+
+inCaseLoggedForEditHeader()
+
+function inCaseLoggedForEditMain() {
+    if (getCookie("acces") === "pass") {
+        const modify = document.createElement('span')
+        modify.innerHTML = "<i class='fa-regular fa-pen-to-square'></i>modifier"
+        modify.classList.add('modify')
+
+        const portfolio = document.querySelector('#portfolio h2')
+        portfolio.append(modify)
+    }
+}
+
+inCaseLoggedForEditMain()
+
+
