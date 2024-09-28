eee27af31288bc2be718325aa8cd2741b94fd964
stephaneM <moyartstephane@gmail.com>
2024-09-28 09:45:36 +0200

    modal add + delete

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,33 +1,48 @@
 function openModal() {
-    const modify = document.querySelector('.modify')
-    modify.addEventListener('click', () => {
-        const modal = document.querySelector('.modal')
-        modal.classList.add('revealModal')
-    
-        const overlay = document.querySelector('.overlay')
-        overlay.classList.add('revealForBlock')
-
+    document.querySelector('.modify').addEventListener('click', () => {
+        document.querySelector('dialog').showModal()
         displayModalContent(1)
     })
 }
 
-openModal()
-
 function closeModal() {
-    const xmark = document.querySelector('.modal .fa-xmark')
-    xmark.addEventListener('click', () => {
-        const modal = document.querySelector('.modal')
-        modal.classList.remove('revealModal')
+    document.querySelector('dialog .fa-xmark')
+    .addEventListener('click', () => document.querySelector('dialog').close())
+} 
+
+
+let token = null
     
-        const overlay = document.querySelector('.overlay')
-        overlay.classList.remove('revealForBlock')
+function getToken() {
+    const cookie = document.cookie.split('; ')
+    const value = cookie
+        .find(c => c.startsWith('acces'))
+    token = value.split('=')[1]
+    return token
+}
+
+function addDeleteIcon(elsContainer, work) {
+    const deleteIcon = document.createElement('i')
+    deleteIcon.classList.add('fa-solid', 'fa-trash-can')
+    deleteIcon.id = work.id
+    elsContainer.append(deleteIcon)
+}
+
+function listenClickToDeleteWork(deleteIcon) {
+    deleteIcon.addEventListener('click', (e) => {
+        getToken()
+        const toDelete = {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        }
+        fetch(`http://localhost:5678/api/works/${e.target.id}`, toDelete)
     })
 }
 
-closeModal()
-
 function displayModal1() {
-    const leftArrow = document.querySelector('.modal .fa-arrow-left')
+    const leftArrow = document.querySelector('dialog .fa-arrow-left')
     leftArrow.classList.remove('revealForBlock')
     fetch("http://localhost:5678/api/works")
                 .then(r => r.json())
@@ -36,20 +51,30 @@ function displayModal1() {
                     modalGallery.classList.add('modalGallery')
                     
                     for (const work of x) {
+                        const elsContainer = document.createElement('div')
+                        elsContainer.classList.add('elsContainer')
                         const img = document.createElement('img')
                         img.src = work.imageUrl
                         img.classList.add('modalGalleryImgs')
 
-                        modalGallery.append(img)
+                        elsContainer.append(img)
+
+                        addDeleteIcon(elsContainer, work)
+                        modalGallery.append(elsContainer)
+
+                        const deleteIcon = elsContainer.querySelector('.fa-trash-can')
+                        listenClickToDeleteWork(deleteIcon)
                     }
                     document.querySelector('.modalBody').innerHTML = ""
+                    
                     document.querySelector('.modalBody').append(modalGallery)
                 })
+                
 }
 
 function displayModal2() {
     // retour en arriere 
-    const leftArrow = document.querySelector('.modal .fa-arrow-left')
+    const leftArrow = document.querySelector('dialog .fa-arrow-left')
     leftArrow.classList.add('revealForBlock')
     leftArrow.addEventListener('click', () => {
             displayModalContent(1)
@@ -57,26 +82,27 @@ function displayModal2() {
     // creation 
     const modalBody = document.querySelector('.modalBody')
     modalBody.innerHTML = `
-    <div class="uploadImgGlobalContainer">
+    <form class="uploadImgGlobalContainer">
         <div class=uploadImg>
             <i class="fa-regular fa-image"></i>
             <label name="img" for="img">+ Ajouter photo</label>
-            <input type="file" id="img" accept="image/*" style="display:none"></input>
+            <input type="file" id="img" name="img" accept="image/*" style="display:none"></input>
             <p>jpg, png : 4mo max</p>
         </div>
         <div class="titleAndCategory">
             <label for="title">Titre</label>
-            <input type="text" id="title"></input>
+            <input type="text" id="title" name="title"></input>
 
             <label for="category">Catégorie</label>
             <select name="category" id="category">
                 <option value="" disabled selected></option> 
             </select>
         </div>
-    </div>
+    </form>
     `
     createCategoryList()
     listenInputImg()
+    enableButton()
 }
 
 function createCategoryList() {
@@ -87,6 +113,7 @@ function createCategoryList() {
             for (const c of x) {
                 const option = document.createElement('option')
                 option.value = c.name
+                option.id = c.id
                 option.textContent = c.name
                 category.append(option)
             }
@@ -123,13 +150,16 @@ function displayModalContent (id) {
         button.textContent = modalX.btnValue
         modalFooter.innerHTML = ""
         modalFooter.append(button)
-        if (id === 2) {
+        if (id === 2 ) {
             button.disabled = true
             button.classList.add('modalButtonDisabled')
+            button.addEventListener('click', async () => await SendFormValuesOnButtonClick())
         }
-        button.addEventListener('click', () => {
-            displayModalContent(2)
-        })
+        if (id === 1) {
+            button.addEventListener('click', () => {
+                displayModalContent(2)
+            })
+        }
     }
 }
 
@@ -142,10 +172,90 @@ function listenInputImg() {
         reader.onload = e => {
             const imgLoaded = document.createElement('img')
             imgLoaded.src = e.target.result
+            console.log(imgLoaded);
+            
             if (imgLoaded.src.length > 0) {
-                document.querySelector('.uploadImg').innerHTML = ""
+                document.querySelectorAll('.uploadImg *')
+                    .forEach(el => el.style.display = "none")
                 document.querySelector('.uploadImg').append(imgLoaded) 
             }
         }
     })
 }
+
+function enableButton() {
+    const imgLd = document.querySelector('.uploadImg img')
+    const title = document.querySelector('.titleAndCategory input')
+    const select = document.querySelector('.titleAndCategory select')
+    const inputFile = document.querySelector('.uploadImg input')
+
+    inputFile.addEventListener('change', () => checkInputs(title, select))
+    title.addEventListener('input', () => checkInputs(title, select))
+    select.addEventListener('change', () => checkInputs(title, select))
+}
+
+function checkInputs(title, select) {
+    const imgLd = document.querySelector('.uploadImg img')
+    const button = document.querySelector('.modalFooter button')
+    if (imgLd && imgLd.src.length > 0 && title.value.length > 0 && select.value.length > 0) {
+        button.disabled = false
+        button.classList.remove('modalButtonDisabled')
+    } else {
+        button.disabled = true
+        button.classList.add('modalButtonDisabled')
+    }
+}
+
+async function SendFormValuesOnButtonClick () {
+    const button = document.querySelector('.modalFooter button')
+    console.log(button);
+    
+    const title = document.querySelector('.titleAndCategory input')
+    console.log(title.value);
+    
+    const select = document.querySelector('.titleAndCategory select')
+
+    // async function generateId() {
+    //     const r = await fetch("http://localhost:5678/api/works")
+    //     const x = await r.json()
+    //     const id = x.length + 1
+    //     console.log(id);
+    //     return id
+    // }
+
+    const categoryId = select.options[select.selectedIndex].id
+    console.log(categoryId);
+    
+    
+    
+    getToken()
+    console.log(token);
+    
+    const imgInput = document.querySelector('.uploadImg input')
+    const formData = new FormData()
+    formData.append('title', title.value)
+    formData.append('image', imgInput.files[0])
+    formData.append('category', categoryId)
+    
+        
+    const toPostNewWork = {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        body: formData 
+    }
+    fetch("http://localhost:5678/api/works", toPostNewWork)
+        .then(r => console.log(r))
+    
+}
+
+
+
+
+
+
+// initialisations ******
+
+openModal()
+closeModal()
